3660f656deb1f4882c2e7ece688dc30c8b68f5c6
Ethan Isola <isolaethan@gmail.com>
2022-03-06 21:28:14 -0600

    commit tree b0af2cd2c5cc2dd630869d343c1ad5d1d34ef79f

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,34 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+
+  get '/messages' do
+  Message.all.order(:created_by).to_json
+  end
+
+  post '/messages' do
+    my_message = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at]
+    )
+    my_message.to_json
+  end
+
+  patch '/messages/:id' do
+    my_message = Message.find(params[:id])
+    my_message.update(body: params[:body])
+    my_message.to_json
+  end
+
+  delete '/messages/:id' do
+    # find the review using the ID
+    my_message = Message.find(params[:id])
+    # delete the review
+    my_message.destroy
+    # send a response with the deleted review as JSON
+    my_message.to_json
+  end
   
 end
